403ca7e6a012c3c525b95e830fbb4901f5df72ac
“ItsYourDik” <203040072@mail.unpas.ac.id>
2022-11-04 11:18:35 +0700

    menyelesaikan video 16 user registration

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
@@ -12,5 +14,23 @@ class RegisterController extends Controller
             'title' => 'Register',
             'active' => 'Register'
         ]);
+    }
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|max:255', //atau bisa pake yang bawah sama aja
+            'username' => ['required', 'min:3', 'max:255', 'unique:users'],
+            'email' => 'required|email:dns|unique:users',
+            'password' => 'required|min:5|max:255'
+        ]);
+
+        // $validatedData['password'] = bcrypt($validatedData['password']);
+        $validatedData['password'] = Hash::make($validatedData['password']);
+
+        User::create($validatedData);
+        // $request->session()->flash('success', 'Registration Successfull! Please Login');
+
+        return redirect('/login')->with('success', 'Registration Successfull! Please Login');
     }
 }
